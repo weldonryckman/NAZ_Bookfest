d7f18f062a80f4aaa0ad404f9b32895a398d3408
weldonryckman <165116001+weldonryckman@users.noreply.github.com>
2026-03-01 14:20:39 -0800

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/1ZxvU8eGyuN9M8GxTU9acKVJv70iC3px_m3EVFsOHN9g/edit#gid=0';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/181w1OsWLp5Vk16enzzT6LIKAjlAYgdyhhiOQBBmElSo/edit?gid=0#gid=0';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
-var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
+var googleApiKey = '77be0ad6514aa77d355496fcbc4f2cd814f903a0';
